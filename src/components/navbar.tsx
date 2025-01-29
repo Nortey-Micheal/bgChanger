29e237d42da68e99da52aa194048006c198784b7
Nortey Michael <norteymicheal91@gmail.com>
2025-01-29 22:05:48 +0000

    add height to logo image in Navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,7 +11,7 @@ type navbarProps = {
 export function Navbar({ image, links}:navbarProps) {
     return (
         <nav className="flex justify-between text-lg text-amber-100 py-3 px-2 bg-blue-700">
-            <img src={image} alt="logo" />
+            <img src={image} className="h-10" alt="logo" />
             <ul className="flex gap-16">
                 {links.map((link, index) => (
                     <li className="bg-amber-300 hover:bg-amber-950 hover:text-amber-100 text-amber-950 py-1 px-4 rounded" key={index}> <a href= {link.url}>{link.label}</a></li>
